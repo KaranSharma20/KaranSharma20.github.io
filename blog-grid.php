d18d6851f42a03d3c10ce59b8b48e9b5a3e487bd
KARAN SHARMA <KARAN987SHA@GMAIL.COM>
2022-06-30 16:18:39 +0530

    Add files via upload

---
+++ blog-grid.php
@@ -0,0 +1,124 @@
+<?php
+include('header.php')
+?>
+  <main id="main">
+
+    <!-- ======= Blog Grid ======= -->
+    <div id="journal-blog" class="text-left  paddsections">
+
+      <div class="container">
+        <div class="section-title text-center">
+          <h2>journal</h2>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="journal-block">
+          <div class="row">
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-1.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-2.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-3.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-1.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-2.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info mb-30">
+
+                <a href="blog-single.php"><img src="assets/img/blog-post-3.jpg" class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                <h4><a href="blog-single.php">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                  <p class="separator">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple
+                  </p>
+
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div><!-- End Blog Grid -->
+
+  </main><!-- End #main -->
+
+<?php
+include('footer.php')
+?>
---
+++ blog-single.php
@@ -0,0 +1,50 @@
+<?php
+include('header.php')
+?>
+  <main id="main">
+
+    <!-- ======= Blog Single ======= -->
+    <div class="main-content paddsection">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="row">
+              <div class="container-main single-main">
+                <div class="col-md-12">
+                  <div class="block-main mb-30">
+                    <img src="assets/img/blog-post-big.jpg" class="img-responsive" alt="reviews2">
+                    <div class="content-main single-post padDiv">
+                      <div class="journal-txt">
+                        <h4><a href="#">Tech Companies Earn More in a Day Than You'll Ever Earn in Your Lifetime</a></h4>
+                      </div>
+                      <div class="post-meta">
+                        <ul class="list-unstyled mb-0">
+                          <li class="author">by:<a href="mailto:karan987sha@gmail.com">Karan Sharma</a></li>
+                          <li class="date">date:<a href="#">June 21, 2022</a></li>
+                        </ul>
+                      </div>
+                      <p class="mb-30">It probably takes you days of work to earn enough to buy an iPhone. But if you’re Apple, it takes just a fraction of a second. Top tech companies are making money at a stunning rate. 
+                      Software company Tipalt examined how much money 50 companies from the Fortune 500 pull in as annual profit to determine which firms make the most money per second. Tech-centric companies dominate the list.
+                       Apple, Microsoft, and Alphabet (Google) are in the top three, taking in $1,752, $1,244, and $1,089 per second, respectively. That translates to $151 billion, $108 billion, and $94 billion a day. A little 
+                       farther down at number 6 is Facebook, making $586 per second. </p>
+                      <p class="mb-30">AT&T and Comcast—not identified as technology companies on this list, but rather as telecom—appear in the eighth and ninth spots with $441 and $414 per second. And Amazon makes the list as
+                         well at number 10, earning $367 every second, though it’s categorized as a retail company. </p>
+                      <blockquote>Any sufficiently advanced technology is equivalent to magic. -Arthur C. Clarke</blockquote>
+                      <p class="mb-30">Tipalt gives the tech sector second place when it comes to profit, behind food and beverages and ahead of telecommunications, financials, and healthcare. But if you categorize AT&T, Comcast,
+                         and Amazon as tech (and we do), then it’s clearly the most profitable sector in the Fortune 500. This doesn’t go for everyone though.
+                      Some companies are losing money every second. GameStop is not the stonk to buy, since the company loses $15 each second. And Tesla is speeding away from profitability, losing $27 per second. Yet it might
+                       never catch up to Uber, which has never turned a profit and loses $270 per second.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Blog Single -->
+
+  </main><!-- End #main -->
+<?php
+include('footer.php')
+?>
